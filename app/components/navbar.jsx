2bd81609e5004009261adc1b029ce839de4c8f69
Daniele Lesniowski Zils <lesniowskidaniele@gmail.com>
2023-05-28 18:06:00 -0400

    added style to navbar

--- app/components/navbar.jsx
+++ app/components/navbar.jsx
@@ -4,13 +4,13 @@ export default function Navbar() {
     return (
 
         <header className="w-full mx-auto px-5 bg-transparent fixed shadow-lg top-0 z-50  ">
-            <div className="justify-between md:items-center md:flex">
+            <div className="md:items-center md:flex md:space-between">
                 <div>
                     <div className="md:py-5 md:block">
-                        <h1 className=" text-xl font-semibold">Becca</h1>
+                        <h1 className="text-xl font-semibold">Becca</h1>
                     </div>
                 </div>
-                <ul className="md:flex md:space-x-6">
+                <ul className="md:flex md:space-x-6 md:justify-around md:w-full">
                   <li className="text-lg">
                     <a href="/">Home</a> 
                   </li>
