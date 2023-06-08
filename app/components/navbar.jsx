e6a80e9a5f27b70496e1cf87d4f0fd4a5dfa0558
Daniele Lesniowski Zils <lesniowskidaniele@gmail.com>
2023-06-07 21:49:28 -0400

    finished navbar mobile style

--- app/components/navbar.jsx
+++ app/components/navbar.jsx
@@ -1,38 +1,82 @@
+'use client';
+
+import { useState } from 'react';
+import {AiOutlineClose, AiOutlineMenu} from 'react-icons/ai'
+
 
 export default function Navbar() {
 
+    const [ nav, setNav ] = useState(false)
+
+    function handleClick() {
+        setNav(!nav)
+    }
+
     return (
-        <nav className="w-full mx-auto ps-20 bg-transparent fixed shadow-lg top-0 z-50 color:white  ">
-            <div className="md:items-center md:flex md:space-between">
-                <div>
-                    <div className="md:py-5 md:block">
-                        <a className="text-2xl font-bold" href="/">Becca</a>
+        <div className="flex justify-between items-center h-24 max-w-[1240px] mx-auto px-4 ">
+                    <div className="">
+                        <a className="text-2xl font-bold w-full" href="/">Becca</a>
                     </div>
-                </div>
-                <ul className="md:flex md:space-x-6 md:justify-around md:w-full ">
-                <li className="text-lg">
+            <ul className="hidden md:flex text-md ">
+                <li className="p-8">
                     <a href="/about">About</a>
                 </li>
-                <li className="text-lg">
+                <li className="p-8">
                     <a href="/violinsOfHope"> Violins of Hope </a>
                 </li>
-                <li className="text-lg">
+                <li className="p-8">
                     <a href="/videos">Media</a>
                 </li>
-                <li className="text-lg">
+                <li className="p-8">
                     <a href="/community">Community Engagement</a>
                 </li>
-                <li className="text-lg">
+                <li className="p-8">
                     <a href="/teaching">Teaching</a>
                 </li>
-                <li className="text-lg">
+                <li className="p-8">
                     <a href="/upcoming">Upcoming Events</a>
                 </li>
-                <li className="text-lg">
+                <li className="p-8">
                     <a href="/contact">Contact</a>
                 </li>
-             </ul>
-            </div>
-        </nav>
+            </ul>
+                <div onClick={handleClick} className='block md:hidden'>
+                    {nav ? <AiOutlineClose size={30} /> : <AiOutlineMenu size={30} />}
+                </div>
+                <div className={nav ? 'fixed left-0 top-0 w-[60%] h-full border-r border-r-gray-300 bg-gray-100 ease-in-out duration-500 z-50': 'ease-in-out duration-500 fixed left-[-100%] z-50'}>
+                <div className="m-4 p-4">
+                    <a className="text-2xl font-bold" href="/">Becca</a>
+                </div>
+                <ul className='uppercase p-4 '>
+                    <li className="text-md p-4 border-b border-r-gray-300">
+                        <a href="/about">About</a>
+                    </li>
+                    <li className="text-md p-4 border-b border-r-gray-300">
+                        <a href="/violinsOfHope"> Violins of Hope </a>
+                    </li>
+                    <li className="p-4 border-b border-r-gray-300" >
+                        <a href="/videos">Media</a>
+                    </li>
+                    <li className="p-4 border-b border-r-gray-300">
+                        <a href="/community">Community Engagement</a>
+                    </li>
+                    <li className="p-4 border-b border-r-gray-300">
+                        <a href="/teaching">Teaching</a>
+                    </li>
+                    <li className="p-4 border-b border-r-gray-300">
+                        <a href="/upcoming">Upcoming Events</a>
+                    </li>
+                    <li className="p-4 border-b border-r-gray-300">
+                        <a href="/contact">Contact</a>
+                    </li>
+                </ul>
+             </div>
+        </div>
     )
 }
+
+
+
+// <nav style: w-full mx-auto ps-20 bg-transparent fixed shadow-lg top-0 z-50 color:white >
+// div below nav: md:items-center md:flex md:space-between
+// fixed left-0 top-0 w-[60%] h-full border-r border-r-gray-300 bg-gray-100
