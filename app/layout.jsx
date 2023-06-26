855cd26a7d9603b141ca129f164d77c1cd1ffc35
Daniele Lesniowski Zils <lesniowskidaniele@gmail.com>
2023-06-25 21:54:10 -0400

    troubleshooting violinsofhope images

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,9 +1,9 @@
 import Navbar from './components/navbar'
 import './globals.css'
-import { Inter } from 'next/font/google'
+// import { Inter } from 'next/font/google'
 
 
-const inter = Inter({ subsets: ['latin'] })
+// const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
   title: 'Rebecca Kasdan',
@@ -16,7 +16,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className} 
+      <body 
         suppressHydrationWarning={true}>
         <Navbar />
         {children}
--- app/violinsOfHope/page.jsx
+++ app/violinsOfHope/page.jsx
@@ -21,25 +21,26 @@ export default function ViolinsOfHope() {
         <div className="relative md:grid md:grid-cols-1 min-h-screen">
             <div className='md:fixed inset-0'>
                 <div className="relative top-24 w-full h-64 md:h-screen">
-                    <div className="object-cover object-center w-full h-full">
                     <Image
                      src={violinsofhope}
                      alt="violinsofhope"
                      priority={true}
+                     placeholder="blur"
+                    className="object-cover object-center w-full h-full"
                     />
-                    </div>
                 </div>
             </div>
             <div className="relative p-8 py-24 md:px-12 md:py-24 md:absolute md:inset-0 md:z-10">
-            <h1 className="py-8 text-2xl font-semibold text-black md:py-8 text-left">Rebecca Kasdan's Doctoral Recital</h1>
+            <h1 className="py-8 text-xl font-semibold text-black md:py-8 text-left">Rebecca Kasdan's Doctoral Recital</h1>
                 <div className="flex flex-col text-center md:items-star">
-                    <YouTube videoId={videoId} opts={videoOptions} className='w-full h-80 2xl:h-[450px] 2xl:w-[450px] mb-8'/>
+                    <YouTube videoId={videoId} opts={videoOptions} className='w-full h-60 2xl:h-[350px] 2xl:w-[350px] 
+                    md:h-[250px] md:w-[250px] mb-8'/>
                     </div>
                     <div className="flex flex-col items-start md:items-star">
                         <iframe 
                             src="https://heyzine.com/flip-book/9b76a89d4b.html" 
                             title="Violins of Hope"
-                            className="w-full h-80 2xl:h-[550px] 2xl:w-[480px] 2xl:px-8"
+                            className="w-full h-80 2xl:h-[450px] 2xl:w-[380px] 2xl:px-8 md:h-[250px] md:w-[250px]"
                         />
             </div>
         </div>
