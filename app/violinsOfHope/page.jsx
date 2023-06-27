9ec1f5ab407af5f99ec2dcb3d65c8a791f77829f
Daniele Lesniowski Zils <lesniowskidaniele@gmail.com>
2023-06-27 11:05:15 -0400

    booklet button

--- app/violinsOfHope/page.jsx
+++ app/violinsOfHope/page.jsx
@@ -32,7 +32,7 @@ export default function ViolinsOfHope() {
             <div className="relative py-24 md:px-12 md:py-24 md:absolute md:inset-0 md:z-10">
             <h1 className="py-8 text-xl font-semibold text-black md:py-8 text-center md:text-left">- Rebecca Kasdan's Doctoral Recital -</h1>
             <p></p>
-                <div className="flex flex-col text-center md:items-star">
+                <div className="flex flex-col text-center md:items-center md:bg-stone-100 w-full 2xl:h-[400px] 2xl:w-[400px] md:pt-8 md:mb-8">
                     <YouTube videoId={videoId} opts={videoOptions} className='w-full h-60 2xl:h-[350px] 2xl:w-[350px] 
                     md:h-[250px] md:w-[250px] mb-8'/>
                     </div>
@@ -40,9 +40,12 @@ export default function ViolinsOfHope() {
                         <iframe 
                             src="https://heyzine.com/flip-book/9b76a89d4b.html" 
                             title="Violins of Hope"
-                            className="w-full h-60 2xl:h-[350px] 2xl:w-[350px] 
+                            className="w-full h-60 2xl:h-[350px] 2xl:w-[400px] 
                             md:h-[250px] md:w-[250px] mb-8"
                         />
+                        <a href="https://heyzine.com/flip-book/9b76a89d4b.html" target="_blank" rel="noreferrer">
+                        <button className="bg-black text-white font-bold py-2 px-4 rounded-full transform hover:scale-110">Full Screen</button>
+                        </a>
             </div>
         </div>
     </div>
