ae5b6a8007beba923a2913971c6253fabb3b8e44
Daniele Lesniowski Zils <lesniowskidaniele@gmail.com>
2023-07-19 17:34:49 -0400

    video added to carousel

--- app/carousel.jsx
+++ app/carousel.jsx
@@ -3,40 +3,65 @@
 import useEmblaCarousel from 'embla-carousel-react';
 import Autoplay from 'embla-carousel-autoplay';
 import Image from 'next/image';
+import Youtube from 'react-youtube';
+import { useState} from 'react';
 
 
 export default function EmblaCarousel(props) {
     const {slides, options} = props;
     const [emblaRef] = useEmblaCarousel(options, [Autoplay()]);
+   
+
+   
 
     // array of images
-    const images = [
-        "/image0.jpeg",
-        "/image1.jpeg",
-        "/image2.jpeg",
-        "/image3.jpeg",
-        "/image4.jpeg",
-    ];
+    const images = [ 
+      {type: 'image', src: "/image0.jpeg", description: "Portland Sea Dogs- National Anthem, Bowdoin International Music Festival"},
+      {type: 'image', src:"/image2.jpeg", description:"Panel Discussion with Midori- Community Engagement in the 21st Century"},
+      {type: 'image', src:"/image3.jpeg", description:"Virginia Tech String Project Masterclass"},
+      {type: 'image', src:"/image4.jpeg", description: "Virginia Tech String Project Masterclass"},
+      {type: 'video', src:"tSYbrkM4sDA", description: "Ukraine Benefit Concert"},
+];
 
     return (
 
-        <div className="">
-          <div className=" overflow-hidden" ref={emblaRef}>
-            <div className=" flex">
-              {slides.map((index) => (
-                <div className="w-screen flex-none" key={index}>
-                  <div className="">
-                    <span>{index + 1}</span>
-                  </div>
+        <div className="relative h-screen overflow-hidden">
+          <div className=" w-full h-full overflow-hidden" ref={emblaRef}>
+            <div className="flex h-full">
+              {images.map((slide, index) => (
+                <div className="w-screen flex-none h-full relative" key={index}>
+                   {slide.type === 'image' ? (
                   <Image 
-                    src={images[index]}
+                    src={images[index].src}
                     alt={`Slide ${index + 1}`}
-                    width = {1000}
-                    height={50}
                     priority={true}
                     quality={85}   
-                    className='w-full h-auto object-cover'
+                    fill
+                    className='absolute z-10 object-contain md:object-cover md:object-center '
                   />
+                   ) : (
+                  <Youtube
+                  videoId={"tSYbrkM4sDA"}  // The YouTube video ID
+                  opts={{
+                    height: '100%',  // You can adjust these options as needed
+                    width: '100%',
+                    playerVars: {
+                      autoplay: 0,
+                      controls: 1,
+                      showinfo: 1,
+                      modestbranding: 1,
+                    },
+                  }}
+                  onReady={event => {
+                    // Access to player in all event handlers via event.target
+                    event.target.pauseVideo();  // Pause the video when it's ready (just in case)
+                  }}
+                  className='absolute z-30 w-full h-full'
+                  />
+                   )}
+                  <div className="absolute z-20 w-full h-full bg-black bg-opacity-20 flex items-end pb-60 justify-center text-white text-lg text-center font-medium md:text-4xl md:p-8">
+                    <p>{images[index].description}</p>
+                  </div>
                 </div>
               ))}
             </div>
