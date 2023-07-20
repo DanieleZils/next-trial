2229c403c58e46546d866e00988efa8744d1e641
Daniele Lesniowski Zils <lesniowskidaniele@gmail.com>
2023-07-20 13:14:45 -0400

    video carousel

--- app/carousel.jsx
+++ app/carousel.jsx
@@ -4,15 +4,46 @@ import useEmblaCarousel from 'embla-carousel-react';
 import Autoplay from 'embla-carousel-autoplay';
 import Image from 'next/image';
 import Youtube from 'react-youtube';
-import { useState} from 'react';
+import { useState, useRef, useEffect, useCallback } from 'react';
 
 
 export default function EmblaCarousel(props) {
-    const {slides, options} = props;
-    const [emblaRef] = useEmblaCarousel(options, [Autoplay()]);
-   
+  const { slides, options } = props;
+  const [emblaRef, emblaApi] = useEmblaCarousel(options);
+  const [intervalId, setIntervalId] = useState(null);
 
-   
+  useEffect(() => {
+    if (emblaApi) {
+      setIntervalId(setInterval(() => {
+        emblaApi.scrollNext();
+      }, 5000));
+    }
+
+    return () => {
+      if (intervalId) clearInterval(intervalId);
+    };
+  }, [emblaApi]);
+
+  const onPlayerStateChange = useCallback((event) => {
+    switch(event.data) {
+      case 1:  // The video started playing
+        if (intervalId) {
+          clearInterval(intervalId);
+          setIntervalId(null);
+        }
+        break;
+      case 0:  // The video ended
+      case 2:  // The video is paused
+        if (!intervalId) {
+          setIntervalId(setInterval(() => {
+            emblaApi.scrollNext();
+          }, 5000));
+        }
+        break;
+      default:
+        break;
+    }
+  }, [intervalId, emblaApi]);
 
     // array of images
     const images = [ 
@@ -56,6 +87,7 @@ export default function EmblaCarousel(props) {
                     // Access to player in all event handlers via event.target
                     event.target.pauseVideo();  // Pause the video when it's ready (just in case)
                   }}
+                  onStateChange={onPlayerStateChange}  // Function gets called when video starts playing
                   className='absolute z-30 w-full h-full'
                   />
                    )}
