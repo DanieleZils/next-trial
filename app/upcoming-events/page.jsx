5903a67a00563562755a871a8b8417bbec82fa35
Daniele Lesniowski Zils <lesniowskidaniele@gmail.com>
2023-09-06 21:18:33 -0400

    upcoming events

--- app/upcoming-events/page.jsx
+++ app/upcoming-events/page.jsx
@@ -34,30 +34,48 @@ export default function UpcomingEvents() {
             </div>
             <div ref={contentRef}
             className="relative p-8 py-24 md:px-12 md:py-24 md:absolute md:inset-0 md:z-10 md:bg-transparent md:mb-10 transition-opacity opacity-0 duration-[2s]">
-                <h2 className=" py-8 text-2xl font-semibold text-black md:text-black md:py-8 text-center md:text-left">Upcoming Events</h2>
+                <h2 className=" py-5 text-2xl font-semibold text-black md:text-black md:py-5 text-center md:text-left">Upcoming Events</h2>
+                <h2 className=" pb-2 text-2xl font-semibold text-black md:text-black text-center md:text-left">2023</h2>
                 <div className='md:text-black md:text-left space-y-6'>
                     {/* Replace these divs with your own event data */}
                     <div className="bg-black bg-opacity-30 md:text-black text-white md:bg-transparent rounded-lg shadow-xl md:shadow-none p-6 md:p-0">
-                        <h3 className="font-bold pb-2">Junior Leadership Initiative : <br/> Discussion on Community Outreach and Engagement in the Arts</h3>
-                        <p className='font-medium text-base pb-2'>August 27, 2 PM, EST (virtual)</p>
-                           <a href="https://volunteermusicians.org/" className='font-bold'>Learn More</a>
+                        <h3 className="font-bold pb-2">MusicWorks Collective Season Opening Concert</h3>
+                        <p className='font-medium text-base pb-2'>September 8, 7 PM</p>
+                        <p className='font-medium text-sm pb-2'>Works by Manuel Ponce, Adrienne Taylor, and Coleridge Taylor Perkinson<br/>
+                        Location : Touisset Point Community Club 10 <br/> Touisset Road, Warren, RI 02885 <br/>
+                        Admission is free; no reservations necessary </p>
+                           <a href="https://communitymusicworks.org/event/musicworks-collective-season-opener-2/" className='font-bold hover:text-maroon '>Learn More</a>
                     </div>
                     <div className="bg-black bg-opacity-30 md:text-black text-white md:bg-transparent rounded-lg shadow-xl md:shadow-none p-6 md:p-0">
                         <h3 className="font-bold pb-2">Community MusicWorks - MusicWorks Collective Concert</h3>
                         <p className='font-medium text-base pb-2'>September 9, 4 PM<br/></p>
                         <p className='font-medium text-sm pb-2'>Works by Manuel Ponce, Adrienne Taylor, and Coleridge Taylor Perkinson<br/>
-                        Location : Temple to Music, Roger Williams Park, F C Greene Memorial Blvd,<br/> Providence, RI 02910</p>
-                           <a href="https://communitymusicworks.org/" className='font-bold'>Learn More</a>
+                        Location : Temple to Music, Roger Williams Park, F C Greene Memorial Blvd,<br/> Providence, RI 02910 <br/>
+                        Admission is free; no reservations necessary </p>
+                           <a href="https://communitymusicworks.org/" className='font-bold hover:text-maroon'>Learn More</a>
                     </div>
                     <div className="bg-black bg-opacity-30 md:text-black text-white md:bg-transparent rounded-lg shadow-xl md:shadow-none p-6 md:p-0">
                         <h3 className="font-bold pb-2">Community MusicWorks Sonata Series</h3>
                         <p className='font-medium text-base pb-2'>October 26, 7 PM </p>
                         <p className='font-medium text-sm pb-2'>Dora Pejačević Violin Sonata No. 1 in D major, Op. 26. With Pianist Andrei Baumann<br/>
                         Location : Music Mansion, 88 Meeting St. Providence, RI 02906</p>
-                           <a href="https://communitymusicworks.org/" className='font-bold'>Learn More</a>
+                           <a href="https://communitymusicworks.org/" className='font-bold hover:text-maroon'>Learn More</a>
+                    </div>
+                    <div className="bg-black bg-opacity-30 md:text-black text-white md:bg-transparent rounded-lg shadow-xl md:shadow-none p-6 md:p-0">
+                        <h3 className="font-bold pb-2">Rhode Island Civic Chorale and Orchestra</h3>
+                        <p className='font-medium text-base pb-2'> November 18, 7 pm </p>
+                        <p className='font-medium text-sm pb-2'>Location : Church of St. Sebastian, Providence, RI 02096</p>
+                           <a href="https://ricco.org/spring-2021/" className='font-bold hover:text-maroon'>Learn More</a>
+                    </div>
+                    <div className="bg-black bg-opacity-30 md:text-black text-white md:bg-transparent rounded-lg shadow-xl md:shadow-none p-6 md:p-0">
+                        <h3 className="font-bold pb-2">Rhode Island Civic Chorale and Orchestra</h3>
+                        <p className='font-medium text-base pb-2'> November 19, 3 pm </p>
+                        <p className='font-medium text-sm pb-2'>Location : St. James Church, Manville, RI 02096</p>
+                           <a href="https://ricco.org/spring-2021/" className='font-bold hover:text-maroon'>Learn More</a>
                     </div>
                 </div>
-                <h2 className=" py-8 text-2xl font-semibold text-black md:text-black md:py-8 text-center md:text-left">Past Events</h2>
+                <h2 className=" py-5 text-2xl font-semibold text-black md:text-black md:py-5 text-center md:text-left">Past Events</h2>
+                <h2 className=" pb-2 text-2xl font-semibold text-black md:text-black text-center md:text-left">2023</h2>
                 <div className='md:text-black md:text-left space-y-6'>
                     {/* Replace these divs with your own event data */}
                     <div className="bg-black bg-opacity-20 text-white md:text-black md:bg-transparent md:shadow-none rounded-lg shadow-xl p-6 md:p-0">
@@ -67,7 +85,7 @@ export default function UpcomingEvents() {
                             August 4, 7:30 PM (EST)<br />
                             203 Lake Tour Road
                             Lake Luzerne, NY</p>
-                           <a href="https://www.luzernemusic.org/2023-season" className='font-bold'>Learn More</a>
+                           <a href="https://www.luzernemusic.org/2023-season" className='font-bold hover:text-maroon'>Learn More</a>
                     </div>
                     <div className="bg-black bg-opacity-20 text-white md:text-black md:bg-transparent rounded-lg shadow-xl md:shadow-none p-6 md:p-0">
                     <h3 className="font-bold pb-2">Easton Chamber Music Festival Faculty Concerts</h3>
@@ -75,13 +93,13 @@ export default function UpcomingEvents() {
                         June 24, 7:30 PM (EST) <br />
                         Covenant Congregational Church
                         204 Center St., Easton MA</p>
-                        <a href="http://eastonchambermusicfestival.org/concerts.html" className='font-bold'>Learn More</a>
+                        <a href="http://eastonchambermusicfestival.org/concerts.html" className='font-bold hover:text-maroon'>Learn More</a>
                     </div>
                     <div className="bg-black bg-opacity-20 text-white md:bg-transparent md:text-black  rounded-lg shadow-xl md:shadow-none p-6 md:p-0">
                     <h3 className="font-bold pb-2">Tonebase Violin</h3>
                         <p className=' font-medium text-base pb-2'>June 20, 3 PM (EST), virtual <br />
                            Sight Reading Skills and Tips.</p>
-                           <a href="https://www.tonebase.co/violin" className='font-bold pb-10'>Learn More</a>
+                           <a href="https://www.tonebase.co/violin" className='font-bold pb-10 hover:text-maroon'>Learn More</a>
                     </div>
             </div>
             </div>
