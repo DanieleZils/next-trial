5fca3f57e71dbca5eb60548a2b6cbe1a419f0983
Daniele Lesniowski Zils <lesniowskidaniele@gmail.com>
2025-12-09 12:25:09 -0500

    changed next.js v and react

--- app/components/navbar.jsx
+++ app/components/navbar.jsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { useState, useEffect } from 'react';
+import Link from 'next/link';
 import {AiOutlineClose, AiOutlineMenu} from 'react-icons/ai'
 
 
@@ -17,43 +18,43 @@ export default function Navbar() {
     return (
         <div className='navbar fixed z-50 w-full flex justify-between items-center h-20 px-4 md:px-12'>
                     <div className="">
-                        <a className="text-2xl font-bold w-full px-2" href="/">Becca Kasdan</a>
+                        <Link className="text-2xl font-bold w-full px-2" href="/">Becca Kasdan</Link>
                     </div>
             <ul className="hidden md:flex xl:text-lg xl:font-semibold sm:text-sm sm:font-semibold sm:text-center">
                 <li className="p-8 group">
-                    <a href="/about" className="relative overflow-hidden inline-block">About
+                    <Link href="/about" className="relative overflow-hidden inline-block">About
                     <span className="absolute inset-x-0 bottom-0 h-0.5 bg-maroon transform scale-x-0 group-hover:scale-x-100 transition duration-300 ease-in-out"></span>
-                    </a>
+                    </Link>
                 </li>
                 <li className="p-8 group">
-                    <a href="/voh-cabaret-concert" className="relative overflow-hidden inline-block"> VoH Cabaret Concert
+                    <Link href="/voh-cabaret-concert" className="relative overflow-hidden inline-block"> VoH Cabaret Concert
                     <span className="absolute inset-x-0 bottom-0 h-0.5 bg-maroon transform scale-x-0 group-hover:scale-x-100 transition duration-300 ease-in-out"></span>
-                    </a>
+                    </Link>
                 </li>
                 <li className="p-8 group">
-                    <a href="/media" className="relative overflow-hidden inline-block">Media
+                    <Link href="/media" className="relative overflow-hidden inline-block">Media
                     <span className="absolute inset-x-0 bottom-0 h-0.5 bg-maroon transform scale-x-0 group-hover:scale-x-100 transition duration-300 ease-in-out"></span>
-                    </a>
+                    </Link>
                 </li>
                 <li className="p-8 group">
-                    <a href="/community-engagement" className="relative overflow-hidden inline-block">Community Engagement
+                    <Link href="/community-engagement" className="relative overflow-hidden inline-block">Community Engagement
                     <span className="absolute inset-x-0 bottom-0 h-0.5 bg-maroon transform scale-x-0 group-hover:scale-x-100 transition duration-300 ease-in-out"></span>
-                    </a>
+                    </Link>
                 </li>
                 <li className="p-8 group">
-                    <a href="/teaching" className="relative overflow-hidden inline-block">Teaching
+                    <Link href="/teaching" className="relative overflow-hidden inline-block">Teaching
                     <span className="absolute inset-x-0 bottom-0 h-0.5 bg-maroon transform scale-x-0 group-hover:scale-x-100 transition duration-300 ease-in-out"></span>
-                    </a>
+                    </Link>
                 </li>
                 <li className="p-8 group">
-                    <a href="/upcoming-events" className="relative overflow-hidden inline-block">Upcoming Events
+                    <Link href="/upcoming-events" className="relative overflow-hidden inline-block">Upcoming Events
                     <span className="absolute inset-x-0 bottom-0 h-0.5 bg-maroon transform scale-x-0 group-hover:scale-x-100 transition duration-300 ease-in-out"></span>
-                    </a>
+                    </Link>
                 </li>
                 <li className="p-8 group">
-                    <a href="/contact" className="relative overflow-hidden inline-block">Contact
+                    <Link href="/contact" className="relative overflow-hidden inline-block">Contact
                     <span className="absolute inset-x-0 bottom-0 h-0.5 bg-maroon transform scale-x-0 group-hover:scale-x-100 transition duration-300 ease-in-out"></span>
-                    </a>
+                    </Link>
                 </li>
             </ul>
                 
@@ -62,43 +63,43 @@ export default function Navbar() {
                 </div>
                 <div className={nav ? 'fixed left-0 top-0 w-[85%] h-full border-r border-r-gray-300 bg-gray-100 ease-in-out duration-500 z-50' : 'ease-in-out duration-500 fixed left-[-100%] z-50'}>
                 <div className="m-4 p-4">
-                    <a className="text-2xl font-bold" href="/">Becca Kasdan</a>
+                    <Link className="text-2xl font-bold" href="/">Becca Kasdan</Link>
                 </div>
                 <ul className='uppercase p-4 '>
                     <li className="text-md p-4 border-b border-r-gray-300 group">
-                        <a href="/about" className="relative overflow-hidden inline-block">About
+                        <Link href="/about" className="relative overflow-hidden inline-block">About
                         <span className="absolute inset-x-0 bottom-0 h-0.5 bg-maroon transform scale-x-0 group-hover:scale-x-100 transition duration-300 ease-in-out"></span>
-                        </a>
+                        </Link>
                     </li>
                     <li className="text-md p-4 border-b border-r-gray-300 group">
-                    <a href="/voh-cabaret-concert" className="relative overflow-hidden inline-block">VoH Cabaret Concert
+                    <Link href="/voh-cabaret-concert" className="relative overflow-hidden inline-block">VoH Cabaret Concert
                     <span className="absolute inset-x-0 bottom-0 h-0.5 bg-maroon transform scale-x-0 group-hover:scale-x-100 transition duration-300 ease-in-out"></span>
-                    </a>
+                    </Link>
                     </li>
                     <li className="p-4 border-b border-r-gray-300 group" >
-                        <a href="/media" className="relative overflow-hidden inline-block">Media
+                        <Link href="/media" className="relative overflow-hidden inline-block">Media
                         <span className="absolute inset-x-0 bottom-0 h-0.5 bg-maroon transform scale-x-0 group-hover:scale-x-100 transition duration-300 ease-in-out"></span>
-                        </a>
+                        </Link>
                     </li>
                     <li className="p-4 border-b border-r-gray-300 group">
-                        <a href="/community-engagement" className="relative overflow-hidden inline-block">Community Engagement
+                        <Link href="/community-engagement" className="relative overflow-hidden inline-block">Community Engagement
                         <span className="absolute inset-x-0 bottom-0 h-0.5 bg-maroon transform scale-x-0 group-hover:scale-x-100 transition duration-300 ease-in-out"></span>
-                        </a>
+                        </Link>
                     </li>
                     <li className="p-4 border-b border-r-gray-300 group">
-                        <a href="/teaching" className="relative overflow-hidden inline-block">Teaching
+                        <Link href="/teaching" className="relative overflow-hidden inline-block">Teaching
                         <span className="absolute inset-x-0 bottom-0 h-0.5 bg-maroon transform scale-x-0 group-hover:scale-x-100 transition duration-300 ease-in-out"></span>
-                        </a>
+                        </Link>
                     </li>
                     <li className="p-4 border-b border-r-gray-300 group">
-                        <a href="/upcoming-events" className="relative overflow-hidden inline-block">Upcoming Events
+                        <Link href="/upcoming-events" className="relative overflow-hidden inline-block">Upcoming Events
                             <span className="absolute inset-x-0 bottom-0 h-0.5 bg-maroon transform scale-x-0 group-hover:scale-x-100 transition duration-300 ease-in-out"></span>
-                            </a>
+                            </Link>
                     </li>
                     <li className="p-4 border-b border-r-gray-300 group">
-                        <a href="/contact" className="relative overflow-hidden inline-block">Contact
+                        <Link href="/contact" className="relative overflow-hidden inline-block">Contact
                         <span className="absolute inset-x-0 bottom-0 h-0.5 bg-maroon transform scale-x-0 group-hover:scale-x-100 transition duration-300 ease-in-out"></span>
-                        </a>
+                        </Link>
                     </li>
                 </ul>
              </div>
